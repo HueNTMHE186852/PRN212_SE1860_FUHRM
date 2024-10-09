61e87e4f312a65ec8db7291521c42e7bb4c6ebec
HueNTMHE186852 <huentmhe186852@fpt.edu.vn>
2024-10-10 00:14:24 +0700

    demo MainWindow

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -1,24 +1,56 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SideMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SideMenu.SelectedItem is ListBoxItem selectedItem)
+            {
+                switch (selectedItem.Content.ToString())
+                {
+                    case "Nhân viên":
+                        MainContentFrame.Navigate(new EmployeeManagement());
+                        break;
+                    case "Bộ phận":
+                        MainContentFrame.Navigate(new DepartmentManagement());
+                        break;
+                    case "Chấm công":
+                        MainContentFrame.Navigate(new AttendanceManagement());
+                        break;
+                    case "Bảng lương":
+                        MainContentFrame.Navigate(new SalaryManagement());
+                        break;
+                    case "Dự án":
+                        MainContentFrame.Navigate(new ProjectManagement());
+                        break;
+                    case "Nghỉ phép":
+                        MainContentFrame.Navigate(new LeaveManagement());
+                        break;
+                    case "Báo cáo thống kê":
+                        MainContentFrame.Navigate(new Report());
+                        break;
+                    case "Tra cứu thông tin":
+                        MainContentFrame.Navigate(new Search());
+                        break;
+                    case "Hệ thống":
+                        MainContentFrame.Navigate(new SystemSettings());
+                        break;
+                    case "Chat":
+                        MainContentFrame.Navigate(new Chat());
+                        break;
+                    default:
+                        MainContentFrame.Navigate(null);
+                        break;
+                }
+            }
         }
     }
 }
