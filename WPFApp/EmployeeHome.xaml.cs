6055df1e5f026b4d36685dd74adac4334a0bd60e
VuHDHE186822 <vuhdhe186822@fpt.edu.vn>
2024-10-29 15:42:51 +0700

    ok

--- WPFApp/EmployeeHome.xaml.cs
+++ WPFApp/EmployeeHome.xaml.cs
@@ -1,27 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Repositories;
+using BusinessObjects;
 
 namespace WPFApp
 {
-    /// <summary>
-    /// Interaction logic for EmployeeHome.xaml
-    /// </summary>
     public partial class EmployeeHome : Window
     {
-        public EmployeeHome()
+        private readonly IAttendanceRepository _attendanceRepository;
+        public ObservableCollection<Attendance> AttendanceRecords { get; set; }
+        public Attendance SelectedAttendance { get; set; }
+
+        public EmployeeHome(IAttendanceRepository attendanceRepository)
         {
             InitializeComponent();
+            _attendanceRepository = attendanceRepository;
+            AttendanceRecords = new ObservableCollection<Attendance>(_attendanceRepository.GetAttendances());
+            AttendanceDataGrid.ItemsSource = AttendanceRecords;
+        }
+
+        private void AttendanceDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AttendanceDataGrid.SelectedItem is Attendance selectedAttendance)
+            {
+                SelectedAttendance = selectedAttendance;
+                AttendanceIdTextBox.Text = selectedAttendance.AttendanceId.ToString();
+                EmployeeNameTextBox.Text = selectedAttendance.Employee.FullName;
+                EmployeeIdTextBox.Text = selectedAttendance.EmployeeId.ToString();
+                DateTextBox.Text = selectedAttendance.Date.ToString();
+                StatusTextBox.Text = selectedAttendance.Status;
+            }
+        }
+
+        private void CheckInButton_Click(object sender, RoutedEventArgs e)
+        {
+            var newAttendance = new Attendance
+            {
+                EmployeeId = 1, // Replace with actual employee ID
+                Date = DateOnly.FromDateTime(DateTime.Now),
+                Status = "Present",
+                OvertimeHours = 0
+            };
+            _attendanceRepository.AddAttendance(newAttendance);
+            AttendanceRecords.Add(newAttendance);
+        }
+
+        private void ClearAttendanceFields()
+        {
+            AttendanceIdTextBox.Clear();
+            EmployeeNameTextBox.Clear();
+            EmployeeIdTextBox.Clear();
+            DateTextBox.Clear();
+            StatusTextBox.Clear();
         }
     }
 }
