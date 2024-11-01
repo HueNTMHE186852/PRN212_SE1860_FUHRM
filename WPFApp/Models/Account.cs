d408d5011079c722dd9766de1c4881a909b856e5
HueNTMHE186852 <huentmhe186852@fpt.edu.vn>
2024-11-01 15:17:26 +0700

    delete and update DB

--- WPFApp/Models/Account.cs
+++ WPFApp/Models/Account.cs
@@ -13,8 +13,6 @@ public partial class Account
 
     public int RoleId { get; set; }
 
-    public virtual ICollection<ActivityLog> ActivityLogs { get; set; } = new List<ActivityLog>();
-
     public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
 
     public virtual Role Role { get; set; } = null!;
